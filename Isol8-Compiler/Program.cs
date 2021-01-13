310a8333a04897e68d9d0e695cfec487ffc5f7d6
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-01-13 19:07:34 +0000

    Add project files.

---
+++ Isol8-Compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Isol8_Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
