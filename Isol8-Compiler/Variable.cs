89a90da5103d7cfe82b5f769cbf227b4d3af93ec
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-01-15 14:58:04 +0000

    Added function and pattern regex patterns

--- Isol8-Compiler/Variable.cs
+++ Isol8-Compiler/Variable.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Isol8_Compiler.Enumerables;
 
 namespace Isol8_Compiler
 {
     class Variable
     {
-
-    }
+        string name;
+        string scope;   //ToDo -- add enum for scope
+        string status;  //ToDo -- as above
+        Types type;     
+        string value;
+    }   
 }
