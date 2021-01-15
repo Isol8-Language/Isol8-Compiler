69cfa8f6e3044aa642a1fc8ad9fe3bade27fb6a8
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-01-15 17:19:44 +0000

    Added patterns

--- Isol8-Compiler/Patterns.cs
+++ Isol8-Compiler/Patterns.cs
@@ -9,7 +9,6 @@ namespace Isol8_Compiler
 {
     public static class Patterns
     {
-        
         public static readonly Regex createPattern = new Regex(@"\b(CREATE)\b \w+ \b(AS)\b \b(INT|STRING)\b \w+;");
         public static readonly Regex lettersOnly = new Regex(@"^[a-zA-Z]+$");
         public static readonly Regex functionPattern = new Regex(@"\b(FUNCTION)\b \w+ \([\s\S].*[\s\S]\)"); // ToDo: Finish
