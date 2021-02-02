69c1ebbe1255bf691ead07266af2c7527b933ddb
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-02-02 15:46:46 +0000

    Comments

--- Isol8-Compiler/WindowsNativeAssembly.cs
+++ Isol8-Compiler/WindowsNativeAssembly.cs
@@ -20,5 +20,31 @@ namespace Isol8_Compiler
                 "mov rsp, rbp\n" +  //Return RSP to original value
                 "pop rbp\n";        //Return RBP to original value
         }
+            //For me, prints hello + hello
+                     /* 
+            EXTERN printf :PROC
+            .DATA
+                msg db "hello"
+            .CODE
+            Initial PROC
+                push rbp
+                mov rbp, rsp
+    
+                sub rsp, 36h
+                ;and spl, 0F0h 
+
+                lea rcx, [msg]
+                lea rax, [rcx+5]
+                mov rdx, [rcx]
+                mov [rax], rdx
+
+                call printf
+
+                mov rsp, rbp
+                pop rbp    
+                ret
+            Initial ENDP
+            END */
+
     }
 }
