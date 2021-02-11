b309714bdd12af3588cbf831139a6ee5ce5cd31b
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-02-11 19:15:25 +0000

    Fixed Kris Algorithm

--- Isol8-Compiler/WindowsNativeAssembly.cs
+++ Isol8-Compiler/WindowsNativeAssembly.cs
@@ -12,35 +12,34 @@ namespace Isol8_Compiler
     {
         public static string CreatePrintFAssembly(string variableName)
         {
-
-            // gonna replace this with list.find to get rid of the loop
-            foreach (Variable x in Parser.variables)
+            int i;
+            for (i = 0; i < Parser.variables.Count; i++)
+                if (variableName == Parser.variables[i].name)
+                    break;
+            
+            if (Parser.variables[i].type == Types.INT)
             {
-                if (x.name == variableName && x.type == Types.INT)
-                {
-                    return "\txor edx, edx\n" +
+                return "\txor edx, edx\n" +
                             $"\tmov eax, [{variableName}]\n" +
                             "\tmov ecx, 0Ah\n" +
                             "\tdiv ecx\n" +
                             "\tadd eax, 30h\n" +
                             "\tadd eax, edx\n" +
-                            $"\tmov r10, [{variableName}]\n"+
+                            $"\tmov edx, [{variableName}]\n" +
                             "\tmov[rsp], r10\n" +
                             $"\tmov[{variableName}], eax\n" +
                             $"\tlea rcx, [{variableName}]\n" +
-                            "\tcall printf\n" + 
-                            "\tmov r10, [rsp + 8]\n" +
-                            $"\tmov[{variableName}], r10\n";
-                }
-                else if (x.name == variableName && x.type == Types.STRING)
-                {
-                    return
-                        $"\tlea rcx, [{variableName}]\n" +
-                        "\tcall printf\n";
-                }
+                            "\tcall printf\n" +
+                            "\tmov edx, [rsp + 8]\n" +
+                            $"\tmov[{variableName}], edx\n";
             }
+            else
+                return
+                $"\tlea rcx, [{variableName}]\n" +
+                "\tcall printf\n";
+  
 
-            return "";
+
 
         }
   
