f56e6277d45570c8ce970ce534cc4d9f8b3915ce
Kristaps Jurkans <kris.jurkans@gmail.com>
2021-02-11 19:09:49 +0000

    Added type checking for printf

--- Isol8-Compiler/WindowsNativeAssembly.cs
+++ Isol8-Compiler/WindowsNativeAssembly.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static Isol8_Compiler.Enumerables;
+
 namespace Isol8_Compiler
 {
     class WindowsNativeAssembly
@@ -11,18 +13,35 @@ namespace Isol8_Compiler
         public static string CreatePrintFAssembly(string variableName)
         {
 
+            // gonna replace this with list.find to get rid of the loop
+            foreach (Variable x in Parser.variables)
+            {
+                if (x.name == variableName && x.type == Types.INT)
+                {
+                    return "\txor edx, edx\n" +
+                            $"\tmov eax, [{variableName}]\n" +
+                            "\tmov ecx, 0Ah\n" +
+                            "\tdiv ecx\n" +
+                            "\tadd eax, 30h\n" +
+                            "\tadd eax, edx\n" +
+                            $"\tmov r10, [{variableName}]\n"+
+                            "\tmov[rsp], r10\n" +
+                            $"\tmov[{variableName}], eax\n" +
+                            $"\tlea rcx, [{variableName}]\n" +
+                            "\tcall printf\n" + 
+                            "\tmov r10, [rsp + 8]\n" +
+                            $"\tmov[{variableName}], r10\n";
+                }
+                else if (x.name == variableName && x.type == Types.STRING)
+                {
+                    return
+                        $"\tlea rcx, [{variableName}]\n" +
+                        "\tcall printf\n";
+                }
+            }
 
-            return "\txor edx, edx\n" +
-                    $"\tmov eax, [{variableName}]\n" +
-                    "\tmov ecx, 0Ah\n" +
-                    "\tdiv ecx\n" +
-                    "\tadd eax, 30h\n" +
-                    "\tadd eax, edx\n";
+            return "";
 
-
-            /*return
-                $"\tlea rcx, [{variableName}]\n" +
-                "\tcall printf\n";*/
         }
   
     }
