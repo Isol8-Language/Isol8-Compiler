fa3def836f7d3432a21f8b640b48f75fa92157a6
Brandon <pokemonbrandon1234@gmail.com>
2021-02-25 13:44:58 +0000

    Auto stash before merge of "Linux" and "origin/master"

--- Isol8-Compiler/Output\OutputName.asm
+++ Isol8-Compiler/Output\OutputName.asm
@@ -1,15 +1,55 @@
+EXTERN printf :PROC
 .DATA
-	exampleInt DD 65535
-	one DD 1
-	two DD 2
+	myInt DD 1
+	helloWorld DB "Hello World", 10, 0
+	myPtr DQ 0
 .CODE
+;START FUNCTION PROLOGUE
 Initial PROC
-	ADD one, 1
-	mov RAX, 1234Ah
+	sub rsp, 28h
+;END FUNCTION PROLOGUE
+
+;START ASSIGNPTR ROUTINE
+	push rax
+	lea rax, [myInt]
+	mov myPtr, rax
+	pop rax
+;END ASSIGNPTR ROUTINE
+
+;START INC/ADD ROUTINE
+	add [myInt], 8
+;END INC/ADD ROUTINE
+
+;START PRINTF ROUTINE
+	lea rcx, [helloWorld]
+	call printf
+;END PRINTF ROUTINE
+
+;START PRINTF ROUTINE
+	xor edx, edx
+	mov eax, [myInt]
+	mov ecx, 0Ah
+	div ecx
+	add eax, 30h
+	add eax, edx
+	mov edx, [myInt]
+	mov[rsp], r10
+	mov[myInt], eax
+	lea rcx, [myInt]
+	call printf
+	mov edx, [rsp + 8]
+	mov[myInt], edx
+;END PRINTF ROUTINE
+
+;START DEL ROUTINE
+	lea rax, myInt
+	mov dword ptr [rax], 0
+;END DEL ROUTINE
+
+;START FUNCTION EPILOGUE
+	mov rax, 0
+	add rsp, 28h
 	ret
 Initial ENDP
-GlobalMaths PROC
-	mov RAX, 0Ah
-	ret
-GlobalMaths ENDP
+;END FUNCTION EPILOGUE
 END
